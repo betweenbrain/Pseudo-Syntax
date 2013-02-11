558536c3f41ad922c27ea51f60c3e2236ba92b45
Matt Thomas <matt@betweenbrain.com>
2013-02-11 11:38:43 -0500

    Update gitignore Initial working version to support {i},{/i} conversion to <i>, </i>

---
+++ pseudosyntax.php
@@ -0,0 +1,20 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       pseudo-syntax.php
+ * Created    2/11/13 11:19 AM
+ * Author     Matt Thomas
+ * Website    http://betweenbrain.com
+ * Email      matt@betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+jimport('joomla.plugin.plugin');
+
+class plgContentPseudosyntax extends JPlugin {
+	function onPrepareContent(&$article, &$params, $limitstart) {
+		$article->title = str_replace(array('{i}', '{/i}'), array('<i>', '</i>'), $article->title);
+	}
+}
